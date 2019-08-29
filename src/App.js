b8e3b312a7da9a90dbe0bd9d76dca30517c879ff
AnChenPLY <1617659868@qq.com>
2019-08-29 18:09:19 +0800

    validate

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 /*
     应用根组件
  */
-import React, { Component } from 'react'
+import React, {Component} from 'react'
+import { message} from 'antd'
+import {BrowserRouter,Switch,Route} from 'react-router-dom'
+import Login from './views/login/login'
+import Admin from './views/admin/admin'
 
-export default class App extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+
+class App extends Component {
+
+  handleClick = () => {
+    message.success('成功啦...');
+  }
+
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route path='/login' component={Login} />          
+          <Route path='/' component={Admin} />          
+        </Switch>
+      </BrowserRouter>
+    )
+  }
 }
+
+export default App
---
+++ src/views/login/login.jsx
@@ -0,0 +1,139 @@
+import React, { Component } from "react";
+import { Form, Icon, Input, Button } from "antd";
+
+import "./login.less";
+import logo from "./images/logo.png";
+class Login extends Component {
+    handleSubmit = e => {
+        e.preventDefault(); //阻止表但提交的默认事件
+
+        //取出输入的相关的数据
+        // const form = this.props.form;
+        // const values = form.getFieldsValue();
+        // const username = form.getFieldValue("username");
+        // const password = form.getFieldValue("password");
+        // console.log(values, username, password);
+
+        //对表单现有字段统一验证
+        this.props.form.validateFields((err, values) => {
+            if (!err) {
+              alert(`发登陆的ajax请求,username=${values.username},password=${values.password}`)
+            }else{
+                //alert('验证失败')
+            }
+          });    
+    };
+
+    /*
+        对密码进行自定义验证
+     */
+    validatePwd=(rule,value,callback)=>{
+        value=value.trim()
+        if(!value){
+            callback('密码必须输入')
+        }else if(value.length<4){
+            callback('密码不能小于4位')
+        }else if(value.length>12){
+            callback('密码不能大于12位')
+        }else if(!/^[a-zA-Z0-9_]+$/.test(value)){
+            callback('密码必须是英文，数字或下划线')
+        }
+        else{
+            callback()//验证通过
+        }
+    }
+    render() {
+        const { getFieldDecorator } = this.props.form;
+        return (
+            <div className="login">
+                <div className="login-header">
+                    <img src={logo} alt="" />
+                    <h1>React项目：后台管理系统</h1>
+                </div>
+                <div className="login-content">
+                    <h1>用户登陆</h1>
+                    <Form onSubmit={this.handleSubmit} className="login-form">
+                        <Form.Item>
+                            {getFieldDecorator("username", {
+                                initialValue: '',
+                                //配置对象：属性名是一些特定的名称
+                                rules: [
+                                    {required:true,whitespace:true, message:'用户名是必须的'},
+                                    {min:4,message:'用户名不能小于4位'},
+                                    {max:12,message:'用户名不能大于12'},
+                                    {pattern:/^[a-zA-Z0-9_]+$/,message:'用户名必须是英文，数字，下划线'}
+                                ]
+                            })(
+                                <Input
+                                    prefix={
+                                        <Icon
+                                            type="user"
+                                            style={{ color: "rgba(0,0,0,.25)" }}
+                                        />
+                                    }
+                                    placeholder="用户名"
+                                />
+                            )}
+                        </Form.Item>
+                        <Form.Item>
+                            {getFieldDecorator("password", {
+                                initialValue: '',
+                                rules: [
+                                    {validator:this.validatePwd}
+                                ]
+                            })(
+                                (
+                                    <Input
+                                        prefix={
+                                            <Icon
+                                                type="user"
+                                                style={{
+                                                    color: "rgba(0,0,0,.25)"
+                                                }}
+                                            />
+                                        }
+                                        placeholder="密码"
+                                    />
+                                )
+                            )}
+                        </Form.Item>
+                        <Form.Item>
+                            <Button
+                                type="primary"
+                                htmlType="submit"
+                                className="login-form-button"
+                            >
+                                登陆
+                            </Button>
+                        </Form.Item>
+                    </Form>
+                </div>
+            </div>
+        );
+    }
+}
+/* 
+    理解Form组件，包含<Form>的组件
+    利用Form.create()包装Form组成一个新的组件
+    新组件会向form组件传递一个强大的属性：属性名：form,属性值对象
+
+
+    高阶函数：
+        接收的参数是函数或参数是函数
+        数组遍历相关的方法/定时器/Promise/高阶组件
+        作用：实现一个更加强大，动态的功能
+
+    高阶组件：
+        本质是一个函数
+        函数是一个组件，返回一个新的组件
+        Form.create()返回的是一个高阶组件
+*/
+const WrappedNormalLoginForm = Form.create()(Login);
+export default WrappedNormalLoginForm;
+/*
+用户名/密码的的合法性要求
+  1). 必须输入
+  2). 必须大于等于4位
+  3). 必须小于等于12位
+  4). 必须是英文、数字或下划线组成
+ */
